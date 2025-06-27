bbd94ef7cf4fe86fbf323565ef28283fcc06899e
Dmitry Gilemkhanov <153055427+dmitryglhf@users.noreply.github.com>
2025-06-27 15:16:05 +0300

    Update setup.sh

--- frameworks/FEDOT/setup.sh
+++ frameworks/FEDOT/setup.sh
@@ -3,54 +3,72 @@ HERE=$(dirname "$0")
 VERSION=${1:-"stable"}
 REPO=${2:-"https://github.com/aimclub/FEDOT.git"}
 PKG=${3:-"fedot"}
-
-# Fix SSL issues
-export GIT_SSL_NO_VERIFY=1
-export PIP_CERT=/etc/ssl/certs/ca-certificates.crt
+CONDA_ENV="amlb"  # Название conda окружения
 
 if [[ "$VERSION" == "latest" ]]; then
     VERSION="master"
 fi
 
-# creating local venv
-. ${HERE}/../shared/setup.sh ${HERE} true
+# Активация conda окружения
+source /opt/conda/etc/profile.d/conda.sh
+conda activate ${CONDA_ENV}
 
-RAWREPO=$(echo ${REPO} | sed "s/github\.com/raw\.githubusercontent\.com/")
+# Настройка SSL для git и pip
+export GIT_SSL_NO_VERIFY=0
+export PIP_CERT=/etc/ssl/certs/ca-certificates.crt
+git config --global http.sslVerify true
+
+# Создание целевой директории
+TARGET_DIR="${HERE}/lib/${PKG}"
+mkdir -p ${TARGET_DIR}
+
+# Функция для безопасной установки через pip
+safe_pip_install() {
+    pip install \
+        --trusted-host pypi.org \
+        --trusted-host files.pythonhosted.org \
+        --cert ${PIP_CERT} \
+        --no-cache-dir \
+        "$@"
+}
+
 if [[ "$VERSION" == "stable" ]]; then
-    PIP install --trusted-host pypi.org --trusted-host files.pythonhosted.org \
-               --cert ${PIP_CERT} \
-               --no-cache-dir -U ${PKG}
+    safe_pip_install -U ${PKG}
     echo GET_VERSION_STABLE
-    VERSION=$(PY -c "${GET_VERSION_STABLE}")
+    VERSION=$(python -c "${GET_VERSION_STABLE}")
 elif [[ "$VERSION" =~ ^[0-9] ]]; then
-    PIP install --trusted-host pypi.org --trusted-host files.pythonhosted.org \
-               --cert ${PIP_CERT} \
-               --no-cache-dir -U ${PKG}==${VERSION}
+    safe_pip_install -U ${PKG}==${VERSION}
 else
-    TARGET_DIR="${HERE}/lib/${PKG}"
     rm -Rf ${TARGET_DIR}
 
     if [[ "$VERSION" =~ ^# ]]; then
-      COMMIT="${VERSION:1}"
+        COMMIT="${VERSION:1}"
     else
-      COMMIT=$(git ls-remote "${REPO}" | grep "refs/heads/${VERSION}" | cut -f 1)
-      DEPTH="--depth 1 --branch ${VERSION}"
+        # Поиск последнего коммита в ветке
+        COMMIT=$(git ls-remote "${REPO}" | grep "refs/heads/${VERSION}" | cut -f 1)
+        DEPTH="--depth 1 --branch ${VERSION}"
     fi
 
-    git clone --recurse-submodules --shallow-submodules ${DEPTH} ${REPO} ${TARGET_DIR}
+    # Клонирование с проверкой SSL
+    GIT_SSL_NO_VERIFY=0 git clone \
+        --recurse-submodules \
+        --shallow-submodules \
+        ${DEPTH} \
+        ${REPO} \
+        ${TARGET_DIR}
+    
     cd ${TARGET_DIR}
     git checkout "${COMMIT}"
-    git config --global http.sslVerify false
-    git submodule update --init --recursive
+    GIT_SSL_NO_VERIFY=0 git submodule update --init --recursive
     cd ${HERE}
-    PIP install --trusted-host pypi.org --trusted-host files.pythonhosted.org \
-               --cert ${PIP_CERT} \
-               -U -e ${TARGET_DIR}
+    
+    safe_pip_install -U -e ${TARGET_DIR}
 fi
 
+# Запись информации об установке
 installed="${HERE}/.setup/installed"
-PY -c "from fedot import __version__; print(__version__)" >> "$installed"
+python -c "from fedot import __version__; print(__version__)" >> "$installed"
 if [[ -n $COMMIT ]]; then
-  truncate -s-1 "$installed"
-  echo "#${COMMIT}" >> "$installed"
+    truncate -s-1 "$installed"
+    echo "#${COMMIT}" >> "$installed"
 fi
